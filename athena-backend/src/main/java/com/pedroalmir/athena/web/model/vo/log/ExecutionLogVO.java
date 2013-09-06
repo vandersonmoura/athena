3f7b75d45f412437f21b6796804ada3c546c92e3
Pedro Almir <petrus.cc@gmail.com>
2013-09-06 20:00:09 -0300

    Criação da classe que representa o log da execução do sistema inteligente. A abordagem inicial gera um relatório mal formatado. Posteriormente deve-se implementar o uso de templates usando Velocity.

---
+++ athena-backend/src/main/java/com/pedroalmir/athena/web/model/vo/log/ExecutionLogVO.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.pedroalmir.athena.web.model.vo.log;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.pedroalmir.athena.AthenaEnvironment;
+import com.pedroalmir.athena.core.report.ExecutionLog;
+
+/**
+ * @author Pedro Almir
+ *
+ */
+public class ExecutionLogVO {
+	private final Long executionTime;
+	private final String executionReport;
+	private final List<String> generatedFilesUserPath;
+	
+	/**
+	 * @param executionLog
+	 */
+	public ExecutionLogVO(ExecutionLog executionLog) {
+		this.executionTime = executionLog.getExecutionTime();
+		this.executionReport = executionLog.getExecutionReport().toString().replaceAll("\n", "<br/>");
+		this.generatedFilesUserPath = new LinkedList<String>();
+		for(String path : executionLog.getGeneratedFilesRealPath()){
+			String[] split = path.split("athena-backend");
+			String userPath = AthenaEnvironment.ATHENA_BASE_URL_II + split[1];
+			generatedFilesUserPath.add(userPath);
+			//System.out.println(userPath);
+		}
+	}
+	
+	/**
+	 * @return the executionTime
+	 */
+	public Long getExecutionTime() {
+		return executionTime;
+	}
+	/**
+	 * @return the executionReport
+	 */
+	public String getExecutionReport() {
+		return executionReport;
+	}
+	/**
+	 * @return the generatedFilesUserPath
+	 */
+	public List<String> getGeneratedFilesUserPath() {
+		return generatedFilesUserPath;
+	}
+	
+}

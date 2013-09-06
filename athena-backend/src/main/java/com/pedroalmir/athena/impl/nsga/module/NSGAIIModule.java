900fc91a4b071654dbaae3455b65124b73d621e6
Pedro Almir <petrus.cc@gmail.com>
2013-09-06 19:55:52 -0300

    Implementação do comportamento do módulo NSGA-II. Inicialmente ele está vinculado ao problema de alocação de equipes, entretanto posteriormente isso será refatorado.

--- athena-backend/src/main/java/com/pedroalmir/athena/impl/nsga/module/NSGAIIModule.java
+++ athena-backend/src/main/java/com/pedroalmir/athena/impl/nsga/module/NSGAIIModule.java
@@ -3,7 +3,14 @@
  */
 package com.pedroalmir.athena.impl.nsga.module;
 
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import jmetal.metaheuristics.nsgaII.NSGAII_main;
+import jmetal.util.JMException;
 
 import com.pedroalmir.athena.core.algorithm.Algorithm;
 import com.pedroalmir.athena.core.component.AbstractBundle;
@@ -14,7 +21,13 @@ import com.pedroalmir.athena.core.put.Input;
 import com.pedroalmir.athena.core.put.Output;
 import com.pedroalmir.athena.core.put.Setting;
 import com.pedroalmir.athena.core.solution.Solution;
+import com.pedroalmir.athena.core.type.numeric.Int;
+import com.pedroalmir.athena.core.type.numeric.Real;
+import com.pedroalmir.athena.core.util.PutUtils;
 import com.pedroalmir.athena.impl.nsga.configuration.NSGAIIConfiguration;
+import com.pedroalmir.athena.impl.teamAllocation.model.Desenvolvedor;
+import com.pedroalmir.athena.impl.teamAllocation.model.Equipe;
+import com.pedroalmir.athena.impl.teamAllocation.model.NSGAIIResult;
 
 /**
  * @author Pedro Almir
@@ -121,12 +134,20 @@ public class NSGAIIModule extends AbstractBundle implements GenericModule {
 
 	@Override
 	public void load(List<Input> inputs, List<Output> outputs, List<Setting> settings) {
-		// TODO Auto-generated method stub
+		this.executionLog.appendLogLine("Carregando módulo NSGA-II...");
+		this.inputs =  inputs;
+		this.outputs = outputs;
+		this.settings = settings;
+		
+		this.loaded = true;
+		this.executionLog.appendLogLine("Módulo NSGA-II carregado com sucesso...");
 	}
 
 	@Override
 	public void load() {
-		// TODO Auto-generated method stub
+		this.executionLog.appendLogLine("Carregando módulo NSGA-II...");
+		this.loaded = true;
+		this.executionLog.appendLogLine("Módulo NSGA-II carregado com sucesso...");
 	}
 
 	@Override
@@ -141,6 +162,62 @@ public class NSGAIIModule extends AbstractBundle implements GenericModule {
 
 	@Override
 	public List<Solution> run() {
+		/* 2. Execute NSGA-II */
+		try {
+			long beginNSGAII = System.currentTimeMillis();
+			this.executionLog.appendLogLine("Início da execução do NSGA-II");
+			NSGAIIResult nsgaiiResult = null;
+			
+			this.executionLog.appendLogLine("Configurando o problema...");
+			Setting restriction =  PutUtils.findSetting("team_size", settings);
+			Setting populationSize =  PutUtils.findSetting("population_size", settings);
+			Setting maximumEvaluations =  PutUtils.findSetting("maximum_evaluations", settings);
+			this.executionLog.setIterations((Integer) ((Int) maximumEvaluations.getType()).getValue());
+			
+			List<Desenvolvedor> desenvolvedores = new LinkedList<Desenvolvedor>();
+			Desenvolvedor desenvolvedor = null; 
+			for(int i = 0; i < this.inputs.get(0).getValues().size(); i++){
+				desenvolvedor = new Desenvolvedor("Desenvolvedor " + i, 
+						((Double) this.inputs.get(0).getValues().get(i).getValue()), 
+						((Double) this.inputs.get(1).getValues().get(i).getValue()));
+				desenvolvedores.add(desenvolvedor);
+			}
+			
+			this.executionLog.appendLogLine("Gerando Equipes...");
+			nsgaiiResult = NSGAII_main.execute(desenvolvedores, 
+					(Integer) ((Int) restriction.getType()).getValue(), 
+					(Integer) ((Int) populationSize.getType()).getValue(), 
+					(Integer) ((Int) maximumEvaluations.getType()).getValue());
+			
+			this.executionLog.appendLogLine("Equipes geradas com sucesso!");
+			int count = 1;
+			for(Equipe equipe : nsgaiiResult.getEquipes()){
+				
+				this.executionLog.appendLogLine("Equipe " + (count++) + ": [" + StringUtils.join(equipe.getNames(), ", ") + "] "
+						+ "Custo Total: " + equipe.getCusto() + " - Produtividade Total: " + equipe.getProdutividade());
+				
+				if(this.outputs.get(0).getName().equalsIgnoreCase("custo")){
+					this.outputs.get(0).addValue(Real.valueOf(equipe.getCusto()));
+					this.outputs.get(1).addValue(Real.valueOf(equipe.getProdutividade()));
+				}else{
+					this.outputs.get(1).addValue(Real.valueOf(equipe.getCusto()));
+					this.outputs.get(0).addValue(Real.valueOf(equipe.getProdutividade()));
+				}
+			}
+			
+			long time = System.currentTimeMillis() - beginNSGAII;
+			nsgaiiResult.setExecutionTime(time);
+			this.executionLog.appendLogLine("Fim da execução do algoritmo...");
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (JMException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
